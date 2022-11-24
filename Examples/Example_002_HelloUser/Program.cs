269724fe49e1d90c9ae05fce597bac42c1a367d1
TrifonovAD <trifonov76@gmail.com>
2022-11-25 00:39:00 +0300

    Add examples <Hello user>

---
+++ Examples/Example_002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// 2. Ввод имени пользователя. Приветствие пользователя
+
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
