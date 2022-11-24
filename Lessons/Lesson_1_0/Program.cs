de1c46453599b65a8feee63d21a351005e437cf4
TrifonovAD <trifonov76@gmail.com>
2022-11-25 02:24:05 +0300

    Add task <Number squared>

---
+++ Lessons/Lesson_1_0/Program.cs
@@ -0,0 +1,9 @@
+﻿// 0. Напишите программу, которая на вход принимает число и
+//    выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Квадрат числа ");
+Console.Write(num);
+Console.Write(" равен ");
+Console.WriteLine(num * num);
