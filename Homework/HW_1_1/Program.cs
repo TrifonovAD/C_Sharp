a41e240bac684e24b30bb5337123a206dbc0c8b6
TrifonovAD <trifonov76@gmail.com>
2022-11-25 04:30:59 +0300

    Add homework 1_1

---
+++ Homework/HW_1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a>b) Console.WriteLine($"max = {a}, min = {b}");
+else if (a<b) Console.WriteLine($"max = {b}, min = {a}");
+else Console.WriteLine($"{a} = {b}");
