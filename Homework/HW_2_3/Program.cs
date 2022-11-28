980646d8eb92cae7413f847db259182c52a0d6e4
TrifonovAD <trifonov76@gmail.com>
2022-11-29 01:49:06 +0300

    Merged all branches. Change homework 2_3

--- Homework/HW_2_3/Program.cs
+++ Homework/HW_2_3/Program.cs
@@ -1,20 +1,70 @@
 ﻿// Задача 3: Напишите программу, которая выводит третью цифру
 // заданного числа или сообщает, что третьей цифры нет.
 
-void ThirdNum (int number)
+// void ThirdNum (int number)
+// {
+//     if (number<100 & number>-100)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//     }
+//     else
+//     {
+//         number = number/100%10;
+//         if (number<0) number = -number;
+//         Console.WriteLine(number);
+//     }
+// }
+
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// ThirdNum(num);
+
+
+// 3. Напишите программу, которая выводит третью цифру
+//    заданного числа или сообщает, что третьей цифры нет.
+
+// ----------------------------------- 1 вариант
+
+
+void ThirdNum(int num)
 {
-    if (number<100 & number>-100)
+    Console.Write($"{num} -> ");
+    if (num < 100)
     {
-        Console.WriteLine("Третьей цифры нет");
+        Console.WriteLine("there is no third digit");
+        return;
     }
-    else
-    {
-        number = number/100%10;
-        if (number<0) number = -number;
-        Console.WriteLine(number);
-    }
+
+    while (num > 999) num /= 10;
+    Console.WriteLine(num % 10);
 }
 
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
-ThirdNum(num);
+ThirdNum(int.Parse(Console.ReadLine()));
+
+
+// ----------------------------------- 2 вариант
+
+
+void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    int tr_num = -1;
+    string txt;
+    
+    while (num > 99)
+    {
+        tr_num = num % 10;        
+        num = num / 10; 
+    }
+    txt = $"{tr_num}";
+    if (tr_num == -1)
+    {
+        txt = "there is no third digit";        
+    }
+    Console.WriteLine(txt);    
+}
+
+ThirdNum(641);
+ThirdNum(32879);
+ThirdNum(32079);
+ThirdNum(78);
