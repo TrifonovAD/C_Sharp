1e01ee5abf82830d2053e2c5f8e9a941831e6d1a
TrifonovAD <trifonov76@gmail.com>
2022-12-01 21:30:09 +0300

    Add Lesson 4

---
+++ Lessons/Lesson_4_0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int SumNums(int num)
+{
+    int allsum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        allsum += i;
+    }
+
+    return allsum;
+}
+
+Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
---
+++ Lessons/Lesson_4_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int NumLength (int number)
+{
+    int length = 0;
+    if (number == 0) return 1;
+    n = Math.Abs(n);
+    while (number > 0)
+    {
+        number = number / 10;
+        length++;
+    }
+    return length;
+
+}
+
+Console.WriteLine (NumLength(int.Parse(Console.ReadLine())));
