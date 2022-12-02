9a21feda5aa6256cdd57095cab063679d410c891
TrifonovAD <trifonov76@gmail.com>
2022-12-02 03:29:26 +0300

    add homework 4

---
+++ Homework/HW_4_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void ExpNum(int a, int b)
+{
+    if (a == 0 && b == 0 || a == 0 && b < 0)
+    {
+        Console.WriteLine($"{a}^{b} -> Решения нет");
+    }
+    else if (a == 0 && b > 0)
+    {
+        Console.WriteLine($"{a}^{b} -> 0");
+    }
+    else if (a != 0 && b == 0)
+    {
+        Console.WriteLine($"{a}^{b} -> 1");
+    }
+    else
+    {
+        int j = Math.Abs(b);
+        int result = 1;
+
+        for(int i = 0; i < j; i++)
+        {
+            result = result * a;
+        }
+        
+        if (b > 0) Console.WriteLine($"{a}^{b} -> {result}");
+        else Console.WriteLine($"{a}^{b} -> 1/{result}");
+    }
+}
+
+Console.Write("A -> ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("B -> ");
+int b = int.Parse(Console.ReadLine());
+
+ExpNum(a, b);
---
+++ Homework/HW_4_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArray (int[] array, int j, int a, int b)
+{
+    for (int i = 0; i < j; i++)
+    {
+        array[i] = new Random().Next(a,b);
+    }
+
+    // return array;
+}
+
+void PrintArray (int[] array, int j)
+{
+    for (int i = 0; i < j-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[j-1]}");
+}
+
+
+Console.Write("Введите количество элементов в массиве: ");
+int lengthArray = int.Parse(Console.ReadLine());
+
+Console.Write("Диапазон чисел: от ");
+int startRandom = int.Parse(Console.ReadLine());
+Console.Write("Диапазон чисел: до ");
+int endRandom = int.Parse(Console.ReadLine());
+
+int[] array = new int[lengthArray];
+
+FillArray (array, lengthArray, startRandom, endRandom);
+PrintArray (array,lengthArray);
