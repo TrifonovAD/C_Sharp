5d9d5d0d95d96e1de3bf98d22a6380a1fce780ca
TrifonovAD <trifonov76@gmail.com>
2022-12-08 19:57:26 +0300

    Add Homework 5.3

--- Homework/HW_5_3/Program.cs
+++ Homework/HW_5_3/Program.cs
@@ -7,17 +7,42 @@ double[] FillArray(int size, int from, int to)
     double[] arr = new double[size];
 
     for (int i = 0; i < size; i++)
-        arr[i] = GetSecureDoubleWithinRange(1,5);
+    {
+        arr[i] = Math.Round(new Random().NextDouble() * 10, 2);
+    }
+
     return arr;
 }
 
 void PrintArray(double[] arr)
 {
+    
     for (int i = 0; i < arr.Length; i++)
+    {
         Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double MaxMin(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    Console.WriteLine($"max -> {max}, min -> {min}");
+    return Math.Round(max - min, 2);
 }
 
 Console.Write("Введите количество элементов массива: ");
-double[] array = FillArray(int.Parse(Console.ReadLine()), 1, 10);
+double[] array = FillArray(int.Parse(Console.ReadLine()), -99, 100);
 
 PrintArray(array);
+
+Console.WriteLine($"Разница между max и min {MaxMin(array)}");
