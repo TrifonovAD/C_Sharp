09a64c49c2e7c7024a7b2976d02c14c407e039c8
TrifonovAD <trifonov76@gmail.com>
2022-12-09 02:31:34 +0300

    Add Homework 6

---
+++ Homework/HW_6_1/Program.cs
@@ -0,0 +1,70 @@
+﻿// // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// // 0, 7, 8, -2, -2 -> 2
+// // 1, -7, 567, 89, 223-> 3
+
+
+// Вариант 1:
+
+int InpNum()
+{
+    string tmp_num;
+    string s_num = "";
+    int num;
+    int pos = 0;
+    Console.Write("Введите число: ");
+    tmp_num = Console.ReadLine();
+    while (int.TryParse(tmp_num, out num))
+    {
+        s_num += $"{tmp_num} ";
+        if (num > 0)
+            pos++;
+        Console.Write("Введите число: ");
+        tmp_num = Console.ReadLine();
+    }
+    Console.Write(s_num);
+    return pos;
+}
+Console.WriteLine($"-> {InpNum()} числа больше 0");
+
+// Вариант 2:
+
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i]);
+//         if (i < arr.Length - 1)
+//             Console.Write(", ");
+//     }
+// }
+
+// int[] InpNumArray()
+// {
+//     string tmp_num;
+//     string s_num = "";
+//     int num;
+//     Console.Write("Введите число: ");
+//     tmp_num = Console.ReadLine()!;
+//     while (int.TryParse(tmp_num, out num))
+//     {
+//         s_num += $"{tmp_num} ";
+//         Console.Write("Введите число: ");
+//         tmp_num = Console.ReadLine()!;
+//     }
+//     s_num = s_num[..^1];
+//     int[] arr = Array.ConvertAll(s_num.Split(), int.Parse);
+//     return arr;
+// }
+// int[] array = InpNumArray();
+
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+
+// PrintArray(array);
+// Console.WriteLine($" -> Кол-во элементов больше 0: {count}");
---
+++ Homework/HW_6_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void CrossPoint(double b1, double k1, double b2, double k2)
+{
+    double x, y;
+    x = Math.Round(((b2 - b1) / (k1 - k2)), 2);
+    y = Math.Round((k1 * x + b1), 2);
+    Console.Write($"({x}; {y})");
+}
+
+Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine()!);
+
+CrossPoint(b1, k1, b2, k2);
