5e588272cf594fd22e76e0baca3b411ed2235a0d
TrifonovAD <trifonov76@gmail.com>
2022-12-13 01:47:38 +0300

    Add Lesson 7

---
+++ Lessons/Lesson_7_0/Program.cs
@@ -0,0 +1,36 @@
+﻿// 1. Задайте двумерный массив размером m×n, 
+//    заполненный случайными целыми числами.
+
+
+int[,] FillArray(int rows, int columns, int from, int to)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j], 6} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] arr_1 = FillArray(row, column,
+                        int.Parse(Console.ReadLine()!),
+                        int.Parse(Console.ReadLine()!));
+
+PrintArray(arr_1);
---
+++ Lessons/Lesson_7_4/Program.cs
@@ -0,0 +1,51 @@
+﻿// 4. Задайте двумерный массив. Введите элемент, и найдите первое его вхождение,
+//    выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+
+
+
+
+int[,] FillArray(int rows, int columns, int from, int to)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j], 6} ");
+        Console.WriteLine();
+    }
+}
+
+string FindNum(int[,] arr, int find)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+            if (arr[i, j] == find)
+                return $"Искомый элемент {find} находится на позиции [{i + 1}, {j + 1}]";
+    return $"Искомый элемент {find} не найден";
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] arr_1 = FillArray(row, column,
+                        int.Parse(Console.ReadLine()!),
+                        int.Parse(Console.ReadLine()!));
+
+PrintArray(arr_1);
+Console.Write("Find number: ");
+int findnum = int.Parse(Console.ReadLine()!);
+
+Console.Write (FindNum(arr_1, findnum));
