6a3fd89a755a0643a7b02435ddb3d186be3f9195
TrifonovAD <trifonov76@gmail.com>
2022-12-17 01:24:50 +0300

    Add Homework 8

---
+++ Homework/HW_8_2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] FillArray(int rows, int columns, int from, int to)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j], 6} ");
+        Console.WriteLine();
+    }
+}
+
+void RowMinSum(int[,] arr)
+{
+    int[] arrsum = new int[arr.GetLength(0)];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arrsum[i] += arr[i, j];
+    }
+    int result = 0;
+    for (int i = 1; i < arr.GetLength(0); i++)
+        if (arrsum[i] < arrsum[result])
+            result = i;
+    Console.WriteLine($"Строка с наименьшей суммой ({arrsum[result]}) -> {result + 1}");
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] arr1 = FillArray(
+    row,
+    column,
+    int.Parse(Console.ReadLine()!),
+    int.Parse(Console.ReadLine()!)
+);
+
+PrintArray(arr1);
+RowMinSum(arr1);
