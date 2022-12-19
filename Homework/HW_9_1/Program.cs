dafdc8f86d3a17a6a95e381ab0f95d926ff8d83d
TrifonovAD <trifonov76@gmail.com>
2022-12-19 22:24:50 +0300

    Add Homework 9

---
+++ Homework/HW_9_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNum(int number)
+{
+    if (number == 0)
+        return;
+    Console.Write($" {number} ");
+    PrintNum(number - 1);    
+}
+int num = int.Parse(Console.ReadLine());
+PrintNum(num);
---
+++ Homework/HW_9_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Summa (int m, int n)
+{
+if (m > n) return 0;
+return Summa (m+1, n) + m;
+}
+
+
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write (Summa (m, n));
